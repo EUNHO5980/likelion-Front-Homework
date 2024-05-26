6bdbdd23d81354b44305f66deed2dd8110dd9764
Eunho <v30319@kangnam.ac.kr>
2024-05-26 20:57:02 +0900

    5/26 게시글 수정

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,10 +4,10 @@ import './NewsItem.css';
 const NewsItem = ({ title, content, source, imageUrl }) => {
   return (
     <div className="news-item">
-      <div className="news-text">
+      <div className="news-content">
         <span className="news-source">{source}</span>
         <h3 className="news-title">{title}</h3>
-        <p className="news-content">{content}</p>
+        <p className="news-description">{content}</p>
       </div>
       <img src={imageUrl} alt="news" className="news-image" />
     </div>
